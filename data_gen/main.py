c2a3a53f3329f9e9ccb9a835a334232a4e639207
aigoncharov <andrey@goncharov.page>
2024-12-09 10:06:46 +0300

    Add output versioning

--- data_gen/main.py
+++ data_gen/main.py
@@ -1,15 +1,18 @@
 from prompt_source import get_prompts
 from domain_pairs import generate_domain_pairs
 import json
+from datetime import datetime
 
 prompts = get_prompts()
 print(len(prompts))
+
+file_name = f"sentiment_split_{datetime.now().strftime('%Y-%m-%d-%H_%M_%S')}.jsonl"
 
 for i, prompt in enumerate(prompts):
     print(f"Generating {i + 1} out of {len(prompts)}")
     gold, unpert, pert = generate_domain_pairs(prompt)
     res = {"prompt_text": prompt, "gold_text": gold[0], "unpert_gen_text": unpert[0], "pert_gen_text": pert[0]}
-    with open("sentiment_split.jsonl", "a") as out:
+    with open(file_name, "a") as out:
         out.write(json.dumps(res))
         out.write("\n")
 
