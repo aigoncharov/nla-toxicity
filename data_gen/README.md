d7bf791ca1ad52d18592c7a6f738b04ea96a128d
aigoncharov <andrey@goncharov.page>
2024-12-11 20:17:53 +0300

    Change data source

--- data_gen/README.md
+++ data_gen/README.md
@@ -4,9 +4,8 @@
 
 1. Classifier: [ROBERTA clickbait](https://s3.amazonaws.com/models.huggingface.co/bert/pplm/discriminators/clickbait_classifier_head.pt)
    1. Download https://s3.amazonaws.com/models.huggingface.co/bert/pplm/discriminators/clickbait_classifier_head.pt to `data` folder
-2. Prompt source: [Cornell Reddit Corpus (small)](https://convokit.cornell.edu/documentation/reddit-small.html)
-   1. Download from https://zissou.infosci.cornell.edu/convokit/datasets/subreddit-corpus/reddit-corpus-small.corpus.zip
-   2. Unpack to `data` folder
+2. Prompt source: [Million news](https://huggingface.co/datasets/rajistics/million-headlines/resolve/main/abcnews-date-text_train.csv)
+   1. Download https://huggingface.co/datasets/rajistics/million-headlines/resolve/main/abcnews-date-text_train.csv to `data` folder
 
 ## Running
 
--- data_gen/prompt_source.py
+++ data_gen/prompt_source.py
@@ -1,53 +1,18 @@
-from convokit import Corpus
+import pandas as pd
 import pathlib
 
-WORD_MIN_COUNT = 8
-CHAR_MIN_COUNT = 30
 LINE_COUNT = 500
 
 
-def count_words(text):
-    return len(text.strip().split(" "))
-
-
-def is_english_letter(char):
-    return char.isalpha() and ord(char) < 128
-
-
-def count_chars(text):
-    cnt = 0
-    for c in text:
-        if is_english_letter(c):
-            cnt += 1
-    return cnt
-
-
 def get_prompts():
-    corpus = Corpus(str(pathlib.Path(__file__).parent.joinpath("data").resolve()))
-    corpus.print_summary_stats()
+    df = pd.read_csv(pathlib.Path(__file__).parent.joinpath("data/abcnews-date-text_train.csv").resolve())
 
-    conversations = []
+    headlines = []
 
-    for convo in corpus.iter_conversations():
-        conversation_text = ""
-        conversation_word_count = 0
-        conversation_char_count = 0
+    for row in df.itertuples():
+        headlines.append(row.headline_text)
 
-        for utt in convo.iter_utterances():
-            if utt.speaker.id == "AutoModerator":
-                continue
-
-            conversation_text += utt.text + " "
-            conversation_word_count += count_words(utt.text)
-            conversation_char_count += count_chars(utt.text)
-
-            if conversation_word_count > WORD_MIN_COUNT and conversation_char_count > CHAR_MIN_COUNT:
-                break
-
-        if conversation_word_count > WORD_MIN_COUNT and conversation_char_count > CHAR_MIN_COUNT:
-            conversations.append(conversation_text)
-
-        if len(conversations) == LINE_COUNT:
+        if len(headlines) == LINE_COUNT:
             break
 
-    return conversations
+    return headlines
